03c14ff0f7395f62da92418feb2a5b0d2dde9c38
DanielRestrepo <danielfeliperestrepoortega@gmail.com>
2025-08-25 20:47:02 -0500

    carrusel y footter
    actualizacion de la pagina

---
+++ script.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll('.carousel-image');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let current = 0;
+
+function showImage(index) {
+  images.forEach(img => img.classList.remove('active'));
+  images[index].classList.add('active');
+}
+
+prevBtn.addEventListener('click', () => {
+  current = (current - 1 + images.length) % images.length;
+  showImage(current);
+});
+
+nextBtn.addEventListener('click', () => {
+  current = (current + 1) % images.length;
+  showImage(current);
+});
